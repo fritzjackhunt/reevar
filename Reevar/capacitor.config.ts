340ddd931d3b0f6ffc80415a025019dd5b7c044f
Aniekan Essiet <essiet.aniekan@yahoo.com>
2025-01-22 05:06:28 +0300

    pem cert

--- Reevar/capacitor.config.ts
+++ Reevar/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.reevar.Reevar',
   appName: 'Reevar',
   webDir: 'www'
 };
